19ec924dcac28dc8fae66074da9abcf2c2e15b11
Waldenth <waldenth@outlook.com>
2020-11-22 22:18:59 +0800

    add output cout code

---
+++ chapter2/carrots.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() { 
+    using namespace std;
+    int carrots;
+
+    carrots = 25;
+    cout << "I have ";
+    cout << carrots;
+    cout << " carrots";
+    cout << endl;
+    carrots = carrots - 1;
+    cout << "Crunch ,crunch. Now I have " << carrots << " carrots\n";
+    system("PAUSE");
+    return 0;
+}
