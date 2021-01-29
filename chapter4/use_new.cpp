276633b03e6d11bd4c1ea22dd9daf1abcdf356e6
Waldenth <waldenth@outlook.com>
2021-01-29 12:27:02 +0800

    chapter4-code

---
+++ chapter4/use_new.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+int main(){
+
+    using namespace std;
+    int nights=1001;
+    int *pt = new int;
+    *pt = 1001;
+    cout << "nights value = ";
+    cout <<nights<<": location "<<&nights<<endl;
+    cout << "int ";
+    cout << "value = " << *pt << ": location = " << pt << endl;
+    double *pd = new double;
+    *pd = 10000001.0;
+
+    cout << "double ";
+    cout << "value = " << *pd << ": location = " << pd << endl;
+    cout << "location of pointer pd: " << &pd << endl;
+    cout << "size of pt = " << sizeof(pt);
+    cout << ": size of *pt = " << sizeof(*pt) << endl;
+    cout << "size  of pd = " << sizeof pd;
+    cout << ": size of *pd = " << sizeof(*pd) << endl;
+    return 0;
+}
