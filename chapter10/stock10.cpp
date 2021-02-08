f54a7bb6da5cc2bb2843a91fe3df17eb23440801
Waldenth <waldenth@outlook.com>
2021-02-08 14:03:08 +0800

    chapter10 part

---
+++ chapter10/stock10.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include "stock10.h"
+
+Stock::Stock() { 
+    std::cout << "Default constructor called\n";
+    company = "no name";
+    shares = 0;
+    share_val = 0.0;
+    total_val = 0.0;
+}
+
+Stock::~Stock() { 
+    std::cout << "Bye, " << company << "!\n"; 
+}
+
+void Stock::buy(long num,double price){
+    using std::cout;
+    if(num<0){
+        cout << "Number of shares purchased can't be negative. "
+             << "Transaction is aborted.\n";
+    }else{
+        shares += num;
+        share_val = price;
+        set_tot();
+    }
+}
+
+void Stock::sell(long num, double price) { 
+    using std::cout; 
+    if(num<0){
+        cout << "Number of shares purchased can't be negative. "
+            << "Transaction is aborted.\n";
+    }else if(num>shares){
+        cout << "You can't sell more than you have! "
+             << "Transaction is aborted.\n";
+    }else{
+        shares -= num;
+        share_val = price;
+        set_tot();
+    }
+}
+
+void Stock::update(double price) { 
+    share_val = price;
+    set_tot();
+}
+
+void Stock::show(){
+    using std::cout;
+    using std::ios_base;
+    ios_base::fmtflags orig = cout.setf(ios_base::fixed, ios_base::floatfield);
+
+    std::streamsize prec = cout.precision(3);
+
+    std::cout << "Company: " << company << " Shares: " << shares << "\n"
+              << " Share Price: $" << share_val;
+    cout.precision(2);
+    cout<< " Total Worth: $" << total_val << "\n";
+
+    cout.setf(orig, ios_base::floatfield);
+    cout.precision(prec);
+}
