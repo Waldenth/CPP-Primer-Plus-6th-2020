3fd3ba6fbb725e0494a8d35f43cd0d9db809a832
Waldenth <waldenth@outlook.com>
2021-10-19 20:19:52 +0800

    add cmake vcpkgRoot

---
+++ cmake/include/hello.h
@@ -0,0 +1,11 @@
+/*声明了Hello类，Hello的方法是print(),*/
+
+#ifndef __HELLO_H__
+#define __HELLO_H__
+
+class Hello {
+   public:
+    void print();
+};
+
+#endif
---
+++ cmake/src/hello.cpp
@@ -0,0 +1,8 @@
+/*实现了Hello::print()*/
+#include <iostream>
+
+#include "Hello.h"
+
+void Hello::print() {
+    std::cout << "Hello Headers!" << std::endl;
+}
