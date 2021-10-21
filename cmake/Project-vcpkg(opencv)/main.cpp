23127a87ed28e5536bbd1d88f98c7056fd41c0cf
Waldenth <waldenth@outlook.com>
2021-10-21 21:06:55 +0800

    add cmake project vcpkg

---
+++ cmake/Project-vcpkg(opencv)/main.cpp
@@ -0,0 +1,16 @@
+#include <opencv2\opencv.hpp>
+using namespace cv;
+using namespace std;
+
+int main() {
+    //读取视频或摄像头
+    VideoCapture capture(0);
+
+    while (true) {
+        Mat frame;
+        capture >> frame;
+        imshow("读取视频", frame);
+        waitKey(30);  //延时30
+    }
+    return 0;
+}
